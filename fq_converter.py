a34cea9b74298458284459d782c5e8124d98f4c5
AlaskaKit <alaska_kit@vivaldi.net>
2020-09-26 13:00:57 +0300

    commit 3: parser fixed. converting module in progress

--- fq_converter.py
+++ fq_converter.py
@@ -1,5 +1,45 @@
+import numpy as np
 
 
 class FqConverter:
 	def __init__(self, num_container, num_channels, num_points):
-		pass
+		self.largest = None
+		self.smallest = None
+		self.source = num_container
+		self.fqs = np.full((num_points, num_channels), 0, dtype=float)
+		
+		for x in range(num_channels):
+			for y in range(num_points):
+				if self.largest is None:
+					self.largest = num_container[y][x]
+				elif num_container[y][x] is None:
+					continue
+				elif num_container[y][x] > self.largest:
+					self.largest = num_container[y][x]
+				else:
+					continue
+		
+		for x in range(num_channels):
+			for y in range(num_points):
+				if self.smallest is None:
+					self.smallest = num_container[y][x]
+				elif num_container[y][x] is None:
+					continue
+				elif num_container[y][x] < self.largest:
+					self.largest = num_container[y][x]
+				else:
+					continue
+		
+		self.gap = self.largest - self.smallest
+		
+	def __convert(self, point):
+		if point is None:
+			return
+		else:
+			ratio = (point - self.smallest) / self.gap
+			fq = 220 * 2 ** ratio
+		return fq
+		
+	def get_fqs(self):
+		self.fqs = 0
+		return self.fqs
--- sonification_cycle.py
+++ sonification_cycle.py
@@ -8,5 +8,7 @@ from fq_converter import FqConverter
 
 if __name__ == '__main__':
 	parsed_sample = ExcelParser('Sample4.xlsx')
-	fqs = FqConverter(parsed_sample.parse(), parsed_sample.channels_num(), parsed_sample.points_num())
+	# print(parsed_sample.parse())
+	converter = FqConverter(parsed_sample.parse(), parsed_sample.channels_num(), parsed_sample.points_num())
+	print(converter.get_fqs())
 	
