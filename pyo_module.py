dc56a777bad464861a7ad8901c13e9ecf3286d62
AlaskaKit <alaska_kit@vivaldi.net>
2020-09-28 23:28:57 +0300

    commit 13: cycle done

--- pyo_module.py
+++ pyo_module.py
@@ -23,8 +23,8 @@ class PyoProcessing:
 				self.source[point] = 0
 			else:
 				continue
-		self.array1 = tuple(float(i) for i in self.source[0:(self.points)])
-		self.array2 = tuple(float(i) for i in self.source[(self.points):(2 * self.points)])
+		self.array1 = tuple(float(i) for i in self.source[0:self.points])
+		self.array2 = tuple(float(i) for i in self.source[self.points:(2 * self.points)])
 		self.array3 = tuple(float(i) for i in self.source[(2 * self.points):])
 		
 		
@@ -38,10 +38,44 @@ class PyoProcessing:
 		s.recordOptions(dur=self.duration, filename=path, fileformat=0, sampletype=1)
 		
 		if self.channels == 1:
-			self.synth1()
+			frequencies1 = iter(self.array1)
+			fqs1 = SigTo(value=self.array1[0], init=200)
+			synth1 = SineLoop(freq=fqs1, mul=.2).out()
+			
+			def pick_new_freq1():
+				try:
+					fqs1.value = next(frequencies1)
+				except StopIteration:
+					fqs1.value = self.array1[-1]
+			
+			pat1 = Pattern(function=pick_new_freq1, time=self.segment).play()
+		
 		elif self.channels == 2:
-			self.synth1()
-			self.synth2()
+			frequencies1 = iter(self.array1)
+			fqs1 = SigTo(value=self.array1[0], init=200)
+			synth1 = SineLoop(freq=fqs1, mul=.2).out()
+			
+			def pick_new_freq1():
+				try:
+					fqs1.value = next(frequencies1)
+				except StopIteration:
+					fqs1.value = self.array1[-1]
+			
+			pat1 = Pattern(function=pick_new_freq1, time=self.segment).play()
+			
+			frequencies2 = iter(self.array2)
+			fqs2 = SigTo(value=self.array2[0], init=200)
+			tab2 = SawTable(order=5).normalize()
+			synth2 = Osc(table=tab2, freq=fqs2, mul=.2).out()
+			
+			def pick_new_freq2():
+				try:
+					fqs2.value = next(frequencies2)
+				except StopIteration:
+					fqs2.value = self.array2[-1]
+			
+			pat2 = Pattern(function=pick_new_freq2, time=self.segment).play()
+			
 		elif self.channels == 3:
 			frequencies1 = iter(self.array1)
 			fqs1 = SigTo(value=self.array1[0], init=200)
@@ -55,11 +89,10 @@ class PyoProcessing:
 			
 			pat1 = Pattern(function=pick_new_freq1, time=self.segment).play()
 			
-			
 			frequencies2 = iter(self.array2)
 			fqs2 = SigTo(value=self.array2[0], init=200)
-			tab = SawTable(order=5).normalize()
-			synth2 = Osc(table=tab, freq=fqs2, mul=.2).out()
+			tab2 = SawTable(order=5).normalize()
+			synth2 = Osc(table=tab2, freq=fqs2, mul=.2).out()
 			
 			def pick_new_freq2():
 				try:
@@ -67,23 +100,21 @@ class PyoProcessing:
 				except StopIteration:
 					fqs2.value = self.array2[-1]
 			
-			pat = Pattern(function=pick_new_freq2, time=self.segment).play()
+			pat2 = Pattern(function=pick_new_freq2, time=self.segment).play()
+			
+			frequencies3 = iter(self.array3)
+			fqs3 = SigTo(value=self.array3[0], init=200)
+			tab3 = SquareTable(order=5).normalize()
+			synth3 = Osc(table=tab3, freq=fqs3, mul=.2).out()
+			
+			def pick_new_freq3():
+				try:
+					fqs3.value = next(frequencies3)
+				except StopIteration:
+					fqs3.value = self.array3[-1]
+			
+			pat3 = Pattern(function=pick_new_freq3, time=self.segment).play()
 		
 		s.recstart()
 		s.start()
 		s.shutdown()
-		
-		
-	def synth3(self):
-		frequencies = iter(self.array3)
-		fqs = SigTo(value=self.array3[0], init=200)
-		tab = SquareTable(order=5).normalize()
-		synth = Osc(table=tab, freq=fqs, mul=.2).out()
-		
-		def pick_new_freq():
-			try:
-				fqs.value = next(frequencies)
-			except StopIteration:
-				fqs.value = self.array3[-1]
-		
-		pat = Pattern(function=pick_new_freq, time=self.segment).play()
