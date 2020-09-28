4053f21b19473ea57e93e2f31bcd3878cfeb6b03
AlaskaKit <alaska_kit@vivaldi.net>
2020-09-28 19:52:49 +0300

    commit 8: fq change implemented

--- pyo_module.py
+++ pyo_module.py
@@ -1,29 +1,35 @@
 from pyo import *
 import os
 import itertools
+from random import randint
 
 hardcode_sample = [220.0, 350.0, 440.0, 546.0, 880.0, 523.0]
 hardcode_duration = 60
 name_sample = "record1"
-
+frequencies = iter(hardcode_sample)
 
 s = Server(audio='offline').boot()
 
 # Path of the recorded sound file.
 path = os.path.join("./wav_files", f"{name_sample}.wav")
 
+# Setting the record options
 s.recordOptions(dur=hardcode_duration, filename=path, fileformat=0, sampletype=1)
 
-#fqs = Linseg(hardcode_sample)
-fqs2 = SigTo(220, time=5, init=200)
+fqs2 = SigTo(value=220, time=8, init=200)
 synth = SineLoop(freq=fqs2, mul=.2).out()
-#synth = SineLoop(freq=fqs, mul=.2).out()
+
+
 def pick_new_freq():
-	fqs2.value = 350
-pat = Pattern(function=pick_new_freq, time=20).play()
+	try:
+		fqs2.value = next(frequencies)
+	except StopIteration:
+		fqs2.value = hardcode_sample[-1]
+		print("The array is over.")
 
 
-#fqs.play()
+pat = Pattern(function=pick_new_freq, time=10).play()
+
 
 
 s.recstart()
