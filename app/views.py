a56c0c9aabb2de797ade46e11a0ff714eba1df10
AlaskaKit <alaska_kit@vivaldi.net>
2020-10-05 15:13:03 +0300

    commit 17(branch): flask sending almost done

--- app/views.py
+++ app/views.py
@@ -1,46 +1,70 @@
 from app import app
-from flask.views import MethodView
-from flask import render_template, request, redirect
+from flask.views import MethodView, View
+from flask import render_template, request, redirect, send_from_directory, abort
+from werkzeug.utils import secure_filename
 import os
 
-app.config["XLS_UPLOADS"] = './app/xls_files'
+from sonification_cycle import SonificationCycle
+
 
 class BasicView(MethodView):
+	
+	def allowed_ext(self, filename):
+		
+		if not "." in filename:
+			return False
+		
+		ext = filename.rsplit(".", 1)[1]
+		
+		if ext.upper() in app.config["ALLOWED_EXTENSIONS"]:
+			return True
+		else:
+			return False
 	
 	def get(self):
 		return render_template("public/index.html")
 	
 	def post(self):
 		if request.form:
-			duration = request.form['set_duration']
+			received_duration = request.form['set_duration']
+			# TODO: проверка типа
+			duration = float(received_duration)
 		else:
 			raise NotImplementedError
 		
 		if request.files:
 			xlsfile = request.files["xlsfile"]
-			xlsfile.save(os.path.join(app.config["XLS_UPLOADS"], xlsfile.filename))
-			return redirect(request.url)
+			
+			if xlsfile.filename == "":
+				raise NotImplementedError
+				
+			if self.allowed_ext(xlsfile.filename):
+				name = secure_filename(xlsfile.filename)
+				xlspath = os.path.join(app.config["XLS_UPLOADS"], name)
+				xlsfile.save(xlspath)
+				# filename = xlspath.split("\\")[-1]
+				process = SonificationCycle(xlspath, duration)
+				wav_path = os.path.join(app.config["WAV_FILES"], f"{name}.wav")
+				return redirect(f"get_wav/{wav_path}")
+			else:
+				raise NotImplementedError
 		else:
 			raise NotImplementedError
-		
-		
-		# uploaded_file = request.files.get('xlsfile')
-		# file_parameters = {
-		# 	'body': uploaded_file.body,
-		# 	'name': uploaded_file.name,
-		# 	'type': uploaded_file.type
-		# }
-		#
-		# file_path = os.path.join("./xls_files", f"{request.files['xlsfile'][0].name}")
-		# with open(file_path, 'wb') as f:
-		# 	f.write(file_parameters['body'])
-		# f.close()
-		# filename = file_path.split("\\")[-1]
-		#
-		# process = SonificationCycle(filename, float(duration))
-		# wav_path = os.path.join("./wav_files", f"{filename}.wav")
-		#
-		# return file_stream(wav_path)
 
-	
+
 app.add_url_rule('/', view_func=BasicView.as_view('BasicView'))
+
+
+@app.route("/get_wav/<path:path>")
+def get_wav(path):
+	
+	try:
+		return send_from_directory(
+			app.config["WAV_FILES"], filename=path, as_attachment=True
+		)
+	except FileNotFoundError:
+		abort(404)
+	
+		
+
+
--- sonification_cycle.py
+++ sonification_cycle.py
@@ -5,17 +5,20 @@ from pyo_module import PyoProcessing
 
 class SonificationCycle:
 	
-	def __init__(self, filename, duration=10):
-		self.filename = filename
+	def __init__(self, sourcepath, duration=10):
+		self.sourcepath = sourcepath
 		self.duration = duration
 		
-		self.parser = ExcelParser(filename)
+		self.parser = ExcelParser(self.sourcepath)
+		
 		self.channels = self.parser.channels_num()
 		self.points = self.parser.points_num()
-		self.source = self.parser.parse()
 		
-		self.converter = FqConverter(self.source, self.channels, self.points)
+		self.fq_source = self.parser.parse()
+		
+		self.converter = FqConverter(self.fq_source, self.channels, self.points)
 		self.frequencies = self.converter.get_fqs()
 		
+		self.filename = self.sourcepath.split("\\")[-1]
 		self.pyo = PyoProcessing(self.frequencies, self.channels, self.points, self.duration, self.filename)
 		self.pyo.process()
